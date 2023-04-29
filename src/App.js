a030d67986c9160091de49cce25b667cac3d0928
BillelBenoudjit <hb_benoudjit@esi.dz>
2023-04-29 22:51:23 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./App.css";
 
 function App() {
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [selectedCol, setSelectedCol] = useState(null);
+
+  const handleRowClick = (rowIndex) => {
+    setSelectedRow(rowIndex);
+  };
+
+  const handleColClick = (colIndex) => {
+    setSelectedCol(colIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5 d-flex flex-column justify-content-center align-items-center">
+      <h1 className="text-center mb-5">Sukar</h1>
+      {[...Array(3)].map((_, rowIndex) => (
+        <Row key={rowIndex} className="mb-3">
+          {[...Array(3)].map((_, colIndex) => (
+            <Col key={colIndex}>
+              <div
+                className={`cell ${
+                  selectedRow === rowIndex || selectedCol === colIndex
+                    ? "selected"
+                    : ""
+                }`}
+                onClick={() => {
+                  handleRowClick(rowIndex);
+                  handleColClick(colIndex);
+                }}
+              />
+            </Col>
+          ))}
+        </Row>
+      ))}
+    </Container>
   );
 }
 
